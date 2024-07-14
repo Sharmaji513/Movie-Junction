32a04df2ecd9a3e1eede1bdf897b21603b59ca64
sachin <sk800968@gmail.com>
2024-07-15 00:19:56 +0530

    Added Popular & Movies Pages

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import Topnav from "../partials/Topnav";
+import Dropdown from "../partials/Dropdown";
+import Loading from "../partials/Loading";
+import axios from '../utils/axios';
+import Cards from "../partials/Cards";
+import { useNavigate } from "react-router-dom";
+// import axios from "axios";
 
-const Movies = () => {
-  return (
-    <div>Movies</div>
-  )
+function Movies() {
+  const navigate = useNavigate();
+  const [category, setCategory] = useState("now_playing");
+  const [movie, setmovie] = useState([]); // Set initial state to an empty array
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+ 
+
+  const GetMovie = async () => {
+    try {
+      const { data } = await axios.get(`/movie/${category}`
+      );
+      setmovie(data.results)
+
+      
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+
+  useEffect(() => {
+    GetMovie();
+  }, [category]);
+
+  return movie?.length > 0 ? (
+    <div className="w-full min-h-screen   py-3 select-auto ">
+        <div className="w-full px-10 flex gap-2  items-center justify-between">
+        <h1
+          onClick={() => navigate(-1)}
+          className="text-2xl flex  gap-5 items-center text-zinc-500 p-4 hover:text-blue-600"
+        >
+          <FaLongArrowAltLeft className="" /> Movies
+        </h1>
+        <Topnav/>
+        <Dropdown
+          title="Category"
+          options={["popular", "top_rated", "upcoming", "now_playing"]}
+          func={(e) => setCategory(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <Cards data={movie} title={`movie`} />
+      </div>
+   
+    </div>
+  ) : (
+    <Loading />
+  );
 }
 
-export default Movies
+export default Movies;
--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import Topnav from "../partials/Topnav";
+import Dropdown from "../partials/Dropdown";
+import Loading from "../partials/Loading";
+import axios from '../utils/axios';
+import Cards from "../partials/Cards";
+import { useNavigate } from "react-router-dom";
+// import axios from "axios";
 
 const Popular = () => {
-  return (
-    <div>Popular</div>
-  )
-}
+  const navigate = useNavigate();
+  const [category, setCategory] = useState("movie");
 
-export default Popular
+  const [popular, setPopular] = useState([]); // Set initial state to an empty array
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+
+  const GetPopular = async () => {
+    try {
+     
+      const { data } = await axios.get(`${category}/popular?language=en-US&page=1`);
+      setPopular(data.results);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    GetPopular();
+  }, [category]);
+
+  return popular?.length > 0 ? (
+    <div className="w-screen h-screen py-3 ">
+      {/* back button  */}
+      <div className="w-full px-10 flex gap-2  items-center justify-between">
+        <h1
+          onClick={() => navigate(-1)}
+          className="text-2xl flex  gap-5 items-center text-zinc-500 p-4 hover:text-blue-600"
+        >
+          <FaLongArrowAltLeft /> Popular{" "}
+        </h1>
+        <Topnav />
+        <Dropdown
+          title={`Category`}
+          options={["movie", "tv"]}
+          func={handleCategoryChange}
+        />
+      </div>
+
+      <div>
+        <Cards data={popular} title={`trending`} />
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Popular;
